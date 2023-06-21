2c4069c1c02c03d1d9f788a7a23c52278cb05bf3
Vlad Soroka <toolbox@provider>
2023-06-21 14:51:48 +0300

    added keypath mutation

--- Sources/Toolbox/extensions/rx/BehaviorRelay+Mutate.swift
+++ Sources/Toolbox/extensions/rx/BehaviorRelay+Mutate.swift
@@ -16,18 +16,26 @@ public extension BehaviorRelay {
         self.accept(x)
     }
     
-    func mutateCommand<T>( _ mutator: @escaping (inout Element, T) -> Void ) -> CommandWith<T> {
-        return CommandWith { t in
-            var x = self.value
-            mutator(&x, t)
-            self.accept(x)
-        }
-    }
-    
     func mutateCommand( _ mutator: @escaping (inout Element) -> Void ) -> Command {
         return Command {
             var x = self.value
             mutator(&x)
+            self.accept(x)
+        }
+    }
+    
+    func mutate<T>( _ keyPath: WritableKeyPath<Element, T> ) -> CommandWith<T> {
+        return CommandWith { t in
+            var x = self.value
+            x[keyPath: keyPath] = t
+            self.accept(x)
+        }
+    }
+    
+    func mutateCommand<T>( _ mutator: @escaping (inout Element, T) -> Void ) -> CommandWith<T> {
+        return CommandWith { t in
+            var x = self.value
+            mutator(&x, t)
             self.accept(x)
         }
     }
