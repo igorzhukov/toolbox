ce96d5aefb32a1d8e6ee650e88a9ccf2631f9e01
Vlad Soroka <toolbox@provider>
2023-11-12 19:05:09 +0200

    appendix

--- Sources/Toolbox/extensions/rx/BehaviorRelay+Mutate.swift
+++ Sources/Toolbox/extensions/rx/BehaviorRelay+Mutate.swift
@@ -32,6 +32,14 @@ public extension BehaviorRelay {
         }
     }
     
+    func mutate<T>( _ keyPath: WritableKeyPath<Element, T?> ) -> CommandWith<T> {
+        return CommandWith { t in
+            var x = self.value
+            x[keyPath: keyPath] = t
+            self.accept(x)
+        }
+    }
+    
     func mutate<T>( _ keyPath: WritableKeyPath<Element, T>, default d: T ) -> CommandWith<T?> {
         return CommandWith { t in
             var x = self.value
